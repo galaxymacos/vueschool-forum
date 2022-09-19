efd420164ab5038258dba8717324446c43941f63
xunruan <xunruan@icloud.com>
2022-09-19 12:57:35 -0400

    initialize a Vue 3 App in main.js
    createApp,
    App.vue as configuration file
    Import Global Vue Component: app.component
    Install Global Vue Plugin: app.use
    Mount Vue to Dom: app.mount('#dom_id')
    
    Use createApp allows us to create two independent Vue instances

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue' // App.vue is the configuration file for the root component
 
-createApp(App).mount('#app')
+// createApp is a function that takes a Vue component as configuration file as an argument
+// and returns a Vue application instance
+// The Vue application instance is then mounted to the DOM element with the id of app
+
+const forumApp = createApp(App)
+// ...additional configuration such as
+
+// 1. registering global components
+forumApp.component('NiceName', {})
+// 2. install plugins
+forumApp.use({}) // e.g. Vue Router, Vuex, etc.
+// 3. Mount the application to the DOM
+forumApp.mount('#app')
+
+// Easy to create a new Vue application instance
+const anotherVueApp = createApp(App)
+anotherVueApp.component('AnotherComponent', {})
+anotherVueApp.use({})
+anotherVueApp.mount('#another-app')
